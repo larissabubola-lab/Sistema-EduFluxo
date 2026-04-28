2a00fe63225ef85035e7c6ea1d00152ae4747ffe
larissa <larissa.bubola@escola.pr.gov.br>
2026-04-28 06:26:23 -0300

    Banco de dados atualizado

--- edufluxo.sql
+++ edufluxo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 16/04/2026 às 22:50
+-- Tempo de geração: 28/04/2026 às 11:19
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -41,9 +41,12 @@ CREATE TABLE `alunos` (
 INSERT INTO `alunos` (`cgm`, `nome`, `email`, `sala`) VALUES
 (67866, 'helen', 'helen@gmail.com', '3° A'),
 (74574, 'marcos', 'matheuspalma0309@gmail.com', '8° C'),
+(457595, 'Pietra', 'svdvj@gmail.com', '6° C'),
+(5659564, 'aluno novo', 'larissa.bubola@escola.pr.gov.br', '7° A'),
 (47554257, 'teste', 'teste@gamial.com', 'outro sala'),
 (54956723, 'elisa', 'larissa.bubola@escola.pr.gov.br', '6° A'),
 (74642472, 'helena', 'helena@escola.pr.gov.br', '2° D'),
+(384624956, 'teodrore', 'maria.souza@gmail.com', '8° C'),
 (784789545, 'banco ta funcionando', 'banco@gamil.com', '7° A'),
 (2147483647, 'teresa', 'larissa.bubola@escola.pr.gov.br', '6° C');
 
@@ -59,6 +62,7 @@ CREATE TABLE `fluxo_saidas` (
   `nome` varchar(100) NOT NULL,
   `serie` varchar(50) NOT NULL,
   `usuario` varchar(100) NOT NULL,
+  `data` varchar(50) NOT NULL,
   `motivo` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -66,10 +70,12 @@ CREATE TABLE `fluxo_saidas` (
 -- Despejando dados para a tabela `fluxo_saidas`
 --
 
-INSERT INTO `fluxo_saidas` (`id`, `cgm`, `nome`, `serie`, `usuario`, `motivo`) VALUES
-(1, 74574, 'marcos', '8° C', 'Larissa', 'teste'),
-(2, 74574, 'marcos', '8° C', 'Larissa', 'O aluno chegou atrasado'),
-(3, 67866, 'helen', '3° A', 'Larissa', 'teste 2');
+INSERT INTO `fluxo_saidas` (`id`, `cgm`, `nome`, `serie`, `usuario`, `data`, `motivo`) VALUES
+(8, 457595, 'Pietra', '6° C', 'Larissa', '19/04/2026', 'O aluno chegou atrasado'),
+(9, 5659564, 'aluno novo', '7° A', 'Larissa', '25/04/2026', 'O aluno saiu mais cedo'),
+(10, 47554257, 'teste', 'outro sala', 'Larissa', '25/04/2026', 'texto grande so para ri ver o que acpntece'),
+(11, 67866, 'helen', '3° A', 'Larissa', '25/04/2026', 'o aluno teve que sair mais cedo devido uma consulta do dentista mas vai voltar mais tarde lorem ipsum'),
+(12, 457595, 'Pietra', '6° C', 'Larissa', '25/04/2026', 'O aluno chegou atrasado');
 
 -- --------------------------------------------------------
 
@@ -83,6 +89,7 @@ CREATE TABLE `ocorrencias` (
   `nome` varchar(100) NOT NULL,
   `serie` varchar(50) NOT NULL,
   `relator` varchar(100) NOT NULL,
+  `data` varchar(50) NOT NULL,
   `tipo` enum('positiva','negativa','','') NOT NULL,
   `motivo` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -91,8 +98,13 @@ CREATE TABLE `ocorrencias` (
 -- Despejando dados para a tabela `ocorrencias`
 --
 
-INSERT INTO `ocorrencias` (`id`, `cgm`, `nome`, `serie`, `relator`, `tipo`, `motivo`) VALUES
-(1, 54956723, 'elisa', '6° A', 'Larissa', 'negativa', 'lorem ipsum');
+INSERT INTO `ocorrencias` (`id`, `cgm`, `nome`, `serie`, `relator`, `data`, `tipo`, `motivo`) VALUES
+(4, 5659564, 'aluno novo', '7° A', 'Larissa', '06/04/2026', 'positiva', 'aaa'),
+(5, 457595, 'Pietra', '6° C', 'Larissa', '06/04/2026', 'negativa', 'ruim'),
+(6, 67866, 'helen', '3° A', 'Larissa', '06/04/2026', 'positiva', 'boa'),
+(7, 5659564, 'aluno novo', '7° A', 'Larissa', '27/04/2026', 'positiva', 'yigrfowrgfrps'),
+(8, 784789545, 'banco ta funcionando', '7° A', 'Larissa', '27/04/2026', 'positiva', 'lotem ipsum'),
+(9, 67866, 'helen', '3° A', 'Larissa', '27/04/2026', 'negativa', 'ocorrencia ruim');
 
 -- --------------------------------------------------------
 
@@ -114,7 +126,9 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`cpf`, `nome`, `email`, `senha`, `permissao`) VALUES
 ('139.840.669.45', 'Larissa', 'larissa.bubola@escola.pr.gov.br', '1234', 0),
+('458605705', 'Amanda Leticia', 'larissa.bubola@escola.pr.gov.br', '1234', 1),
 ('463.443.975.23', 'Pedro', 'pedro.lopes@escola.pr.gov.br', '1234', 1),
+('4765504264', 'Sandra', 'aaaa@gamil.com', '1234', 2),
 ('563.345.656.00', 'Maria', 'maria.souza@gmail.com', '1234', 2);
 
 --
@@ -153,13 +167,13 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `fluxo_saidas`
 --
 ALTER TABLE `fluxo_saidas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de tabela `ocorrencias`
 --
 ALTER TABLE `ocorrencias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
